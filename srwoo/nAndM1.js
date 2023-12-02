25882eaabe2ecc835bddaeb0c313cd43ebe4ef74
srwoo <rhak12@naver.com>
2023-12-02 13:50:27 +0900

    Baekjoon Online Judge - N과 M (1) (코드 리팩토링)
    by : 우세림 <selim0915@gmail.com>

--- srwoo/nAndM1.js
+++ srwoo/nAndM1.js
@@ -2,36 +2,27 @@ const fs = require('fs');
 const { listenerCount } = require('process');
 // const input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
 const input = `
-4 2
+5 3
 `.toString().trim().split('\n');
 
-const recursive = (n, m, depth) => {
+const [n, m] = input[0].split(' ').map(Number);
+let arr = [];
+let visit = [...Array(n)].fill(false);
+
+const recursive = (depth) => {
     if(depth === m){
-        let res = ''
-        for(let val of arr){
-            res += val + ' ';
-        }
-        console.log(res);
+        console.log(arr.join(' '));
         return;
-        // for(let val of arr){
-        //     process.stdout.write(`${val} `);
-        // }
-        // console.log('');
-        // return;
     }
 
     for(let i=1; i<=n; i++){
-        if(!visit[i]){
-            arr[depth] = i;
-            visit[i] = true;
-            recursive(n, m, depth+1);
-            visit[i] = false;
-        }
+        if(visit[i]) continue;
+        
+        arr[depth] = i;
+        visit[i] = true;
+        recursive(depth+1);
+        visit[i] = false;
     }
 }
 
-const [n, m] = input[0].split(' ').map(Number);
-let arr = [...Array(m)].fill(0);
-let visit = [...Array(n)].fill(false);
-
-recursive(n, m, 0);
+recursive(0);
